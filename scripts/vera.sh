d9e10f9ed62be4b36dda647e6c1467a7e1145322
Steve McQuaid <steve@stevemcquaid.com>
2018-01-11 23:28:13 +0000

    Change to otp.vc & expand shortened flags

--- scripts/vera.sh
+++ scripts/vera.sh
@@ -2,6 +2,6 @@
 set -ex
 
 # Run veracrypt and prompt for password
-/Applications/VeraCrypt.app/Contents/MacOS/VeraCrypt -t -k "" --protect-hidden=no /Users/steve/Dropbox/1Haktop/dev/vera/otp.txt  /Volumes/otp
+/Applications/VeraCrypt.app/Contents/MacOS/VeraCrypt --text --keyfiles "" --protect-hidden=no /Users/steve/Dropbox/1Haktop/dev/vera/otp.vc  /Volumes/otp
 
 docker run -it --rm -v /Volumes/otp/:/Volumes/otp/ stevemcquaid/otp:latest go-wrapper run -config /Volumes/otp/config.csv 
